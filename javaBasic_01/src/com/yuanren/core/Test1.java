35d29f880d3ca79130cd8df47b5bddd8cd337cd6
guoqingduan <you@example.com>
2021-06-08 10:19:52 +0800

    homework

---
+++ javaBasic_01/src/com/yuanren/core/Test1.java
@@ -0,0 +1,81 @@
+package com.yuanren.core;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("哈哈哈哈");
+
+        int Int=12;
+
+        int a= 5678;
+        int ge=a%10;
+        int shi=(a-ge)/10%10;
+        int bai=(a-(shi*10+ge))/100%10;
+        int qian=a/1000;
+        int number=ge+shi+bai+qian;
+        System.out.println(number);
+
+        //补码    反码   正数  负数   格式化
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("输入第一个年龄：");
+        int age1=scanner.nextInt();
+        if (age1<=3&&age1>=0){
+            System.out.println("婴幼儿。");
+        }else if(age1<=12&&age1>=3){
+            System.out.println("儿童。");
+        }else if(age1<=17&&age1>=12){
+            System.out.println("青年。");
+        }else if(age1>=28){
+            System.out.println("老年人。");
+        }
+        System.out.println("输入第二个年龄：");
+        int age2=scanner.nextInt();
+        if (age2<=3&&age2>=0){
+            System.out.println("婴幼儿。");
+        }else if(age2<=12&&age2>=3){
+            System.out.println("儿童。");
+        }else if(age2<=17&&age2>=12){
+            System.out.println("青年。");
+        }else if(age2>=28){
+            System.out.println("老年人。");
+        }
+        System.out.println("输入第三个年龄：");
+        int age3=scanner.nextInt();
+        if (age3<=3&&age1>=0){
+            System.out.println("婴幼儿。");
+        }else if(age3<=12&&age3>=3){
+            System.out.println("儿童。");
+        }else if(age3<=17&&age3>=12){
+            System.out.println("青年。");
+        }else if(age3>=28){
+            System.out.println("老年人。");
+        }
+        System.out.println("输入第四个年龄：");
+        int age4=scanner.nextInt();
+        if (age4<=3&&age1>=0){
+            System.out.println("婴幼儿。");
+        }else if(age4<=12&&age4>=3){
+            System.out.println("儿童。");
+        }else if(age4<=17&&age4>=12){
+            System.out.println("青年。");
+        }else if(age4>=28){
+            System.out.println("老年人。");
+        }
+        System.out.println("输入第五个年龄：");
+        int age5=scanner.nextInt();
+        if (age5<=3&&age5>=0){
+            System.out.println("婴幼儿。");
+        }else if(age5<=12&&age5>=3){
+            System.out.println("儿童。");
+        }else if(age5<=17&&age5>=12){
+            System.out.println("青年。");
+        }else if(age5>=28){
+            System.out.println("老年人。");
+        }
+        
+
+    }
+}
--- javaBasic_03/src/com/yuanren/testFor.java
+++ javaBasic_03/src/com/yuanren/testFor.java
@@ -1,7 +1,96 @@
 package com.yuanren;
 
+import java.util.Scanner;
+
 public class testFor {
     public static void main(String[] args){
+        /**
+         * 手动输入两个数number1,number2求这两个数中的所有偶数，求偶属的个数
+         */
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("请输入第一个数：");
+        int number1=scanner.nextInt();
+        System.out.println("请输入第二个数：");
+        int number2=scanner.nextInt();
+        int max=number1;
+        if(max<number2){
+            max=number2;
+        }
+        int min=number2;
+        if(min>number1){
+            min=number1;
+        }
+        int count=0;
+        for(int i=min;min<=max;min++){
+            if(min%2==0){
+                System.out.println("偶数是："+min);
+                count++;
+            }
+        }
+        System.out.println("有"+count+"个偶数。");
+
+
+        /**
+         * 手动输入两个数number1,number2求这两个数中的所有奇数和
+         */
+        System.out.println("请输入第一个数：");
+        int number3=scanner.nextInt();
+        System.out.println("请输入第二个数：");
+        int number4=scanner.nextInt();
+        max=number3;
+        if(max<number4){
+            max=number4;
+        }
+        min=number4;
+        if(min>number3){
+            min=number3;
+        }
+        int sum=0;
+        for(int i=min;min<=max;min++){
+            if(min%2==1){
+                System.out.println("奇数是："+min);
+                sum+=min;
+            }
+        }
+        System.out.println(number3+"和"+number4+"之间的奇数和为"+sum);
+
+        /**
+         * 手动输入三个数，求这三个数中的最大值
+         */
+        System.out.println("请输入第一个数：");
+        number1=scanner.nextInt();
+        System.out.println("请输入第二个数：");
+        number2=scanner.nextInt();
+        System.out.println("请输入第三个数：");
+        number3=scanner.nextInt();
+        max=number1;
+        if(max<number2){
+            max=number2;
+        }
+        if(max<number3){
+            max=number3;
+        }
+        System.out.println("三个数中的最大值为"+max);
+        /**
+         * 手动输入0~9之间的数，求出现的偶数的次数，如果输入的数小于0或者是大于9，程序结束
+         */
+        int cout=0;
+        while(true){
+            System.out.println("请输入一个0~9的数字");
+            number1=scanner.nextInt();
+           if(number1>=0&&number1<=9) {
+               if (number1 % 2 == 0) {
+                   cout++;
+               }
+               System.out.println("偶数出现了" + cout + "次。");
+           }else {
+               System.out.println("您输入的数据过大!");
+               break;
+           }
+        }
+
+
 
     }
 }
+
